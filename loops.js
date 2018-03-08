adcbd453ceb94e040bca92b0ddf91f0a687cf5c9
MichelleHuang Huang <mh3793@stern.nyu.edu>
2018-03-08 17:36:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5
   }
  
-do {
-  array.pop();
-} while (array.length > 0 && maybeTrue());
-}
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  }
